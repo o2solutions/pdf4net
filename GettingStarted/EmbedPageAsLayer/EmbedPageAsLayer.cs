173c3c98fae86156eba6f53381ceb65425149b4b
O2 Solutions Support <techsupport@o2sol.com>
2020-08-24 22:37:34 +0300

    Samples

---
+++ GettingStarted/EmbedPageAsLayer/EmbedPageAsLayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using O2S.Components.PDF4NET.Core;
+using O2S.Components.PDF4NET.Graphics;
+using O2S.Components.PDF4NET.Graphics.OptionalContent;
+
+namespace O2S.Components.PDF4NET.Samples
+{
+    public class EmbedPageAsLayer
+    {
+        public static void Main(string[] args)
+        {
+            // Extract the page content from the source file.
+            FileStream stream = File.OpenRead("input.pdf");
+            PDFFile source = new PDFFile(stream);
+            PDFPageContent pageContent = source.ExtractPageContent(0);
+            stream.Close();
+
+            PDFFixedDocument document = new PDFFixedDocument();
+            document.OptionalContentProperties = new PDFOptionalContentProperties();
+            PDFPage page = document.Pages.Add();
+
+            // Create an optional content group (layer) for the extracted page content.
+            PDFOptionalContentGroup ocg = new PDFOptionalContentGroup();
+            ocg.Name = "Embedded page";
+            ocg.VisibilityState = PDFOptionalContentGroupVisibilityState.AlwaysVisible;
+            ocg.PrintState = PDFOptionalContentGroupPrintState.NeverPrint;
+            // Draw the extracted page content in the layer
+            page.Canvas.BeginOptionalContentGroup(ocg);
+            page.Canvas.DrawFormXObject(pageContent, 0, 0, page.Width, page.Height);
+            page.Canvas.EndOptionalContentGroup();
+
+            // Build the display tree for the optional content
+            PDFOptionalContentDisplayTreeNode ocgNode = new PDFOptionalContentDisplayTreeNode(ocg);
+            document.OptionalContentProperties.DisplayTree.Nodes.Add(ocgNode);
+
+            using (FileStream output = File.Create("EmbedPageAsLayer.pdf"))
+            {
+                document.Save(output); 
+            }
+        } 
+    }
+}
---
+++ GettingStarted/EmbedPageAsLayer/README.md
@@ -0,0 +1,2 @@
+The `EmbedPageAsLayer` sample shows how to take an existing PDF page and embed it in another page as a PDF layer.
+ 
