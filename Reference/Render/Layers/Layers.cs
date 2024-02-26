215e8f043f6b6e7fb9d41393614187e105baa63e
O2 Solutions Support <techsupport@o2sol.com>
2024-02-26 17:16:33 +0200

    Added Layers sample

---
+++ Reference/Render/Layers/Layers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using O2S.Components.PDF4NET;
+using O2S.Components.PDF4NET.Rendering;
+using O2S.Components.PDF4NET.Rendering.RenderingSurfaces;
+
+namespace O2S.Components.PDF4NET.Samples
+{
+    class Layers
+    {
+        public static void Main(string[] args)
+        {
+            FileStream pdfStream = File.OpenRead("..\\..\\..\\..\\..\\..\\SupportFiles\\layers.PDF");
+            PDFFixedDocument document = new PDFFixedDocument(PDFStream);
+            pdfStream.Dispose();
+
+            PDFPageRenderer pageRenderer = new PDFPageRenderer(document.Pages[0]);
+
+            PDFRendererSettings settings = new PDFRendererSettings(96, 96);
+            settings.RenderingSurface = pageRenderer.CreateRenderingSurface<PDFArgbRenderingSurface<int>>(settings.DpiX, settings.DpiY);
+
+            FileStream imageStream = File.Create("Layers.AllContent.tiff");
+            // By default all page content is rendered, layers visibility is ignored.
+            pageRenderer.ConvertPageToImage(imageStream, PDFPageImageFormat.Tiff, settings);
+            imageStream.Flush();
+            imageStream.Close();
+
+            // Render only the layers that are displayed when the document is viewed.
+            settings.LayerRenderTarget = PDFLayerRenderTarget.View;
+            imageStream = File.Create("Layers.View.tiff");
+            pageRenderer.ConvertPageToImage(imageStream, PDFPageImageFormat.Tiff, settings);
+            imageStream.Flush();
+            imageStream.Close();
+
+            // Render only the layers that are displayed when the document is printed.
+            settings.LayerRenderTarget = PDFLayerRenderTarget.Print;
+            imageStream = File.Create("Layers.Print.tiff");
+            pageRenderer.ConvertPageToImage(imageStream, PDFPageImageFormat.Tiff, settings);
+            imageStream.Flush();
+            imageStream.Close();
+
+            Console.WriteLine("Layers sample completed.");
+        }
+    }
+}
---
+++ Reference/Render/Layers/README.md
@@ -0,0 +1,2 @@
+The `Layers` sample shows how to convert a PDF page to image and consider layer visiblity in the process.
+By default all page content is rendered. The `PDFRendererSettings.LayerRenderTarget` property lets specify what kind of layers should be considered when page is rendered. 
